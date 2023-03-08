af8609d43fd5187ead842c38314921b9aaa85058
JesusAlcantara <jesusalcantaragomez11@gmail.com>
2023-03-08 17:47:34 +0100

    characters page

--- components/CharactersBody.vue
+++ components/CharactersBody.vue
@@ -1,17 +1,52 @@
 <template>
-    <div>
-        <h2>Characters Body</h2>
+    <div class="d-flex align-content-start flex-wrap contenedor">
+        <div
+            v-for="character in items"
+            :key="character.key"
+            class="card m-2"
+            style="width: 18rem;"
+        >
+            <img :src="getImage(character.image)" class="card-img-top" :alt="character.name">
+            <div class="card-body">
+                <h5 class="card-title">{{ character.name }}</h5>
+                <p class="card-text">{{ character.description }}</p>
+                <a href="#" class="btn btn-primary">Ver más</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import characters from '~/assets/characters.json';
 export default {
-
+    computed: {
+        items () {
+            return characters.map((item) => {
+                return item
+            })
+        }
+    },
+    methods: {
+        getImage(img) {
+            return require(`../public/images/charactersImg/${img}`);
+        }
+    }
 }
 </script>
 
 <style scoped>
-    div {
+    .contenedor {
         background-color: white;
+    }
+    .card-title {
+        font-family: fantasy;
+    }
+    .card-text {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+    .card-img-top {
+        height: 400px;
+        width: 100%;
+        object-fit: cover;
     }
 </style>
--- components/Heading.vue
+++ components/Heading.vue
@@ -8,30 +8,30 @@
             :key="item"
             class="nav-item d-flex"
           >
-            <nuxt-link class="nav-link text-white" aria-current="page" :to="`/${item.route}`">{{ item }}</nuxt-link>
+            <nuxt-link class="nav-link text-white" aria-current="page" :to="`${item.url}`">{{ item.name }}</nuxt-link>
           </li>
         </ul>
-        <img src="../assets/images/bolaDragon7.png" alt="dragonBall" width="50" height="50" class="img-thumbnail rounded float-right m-2">
+        <img src="../public/images/bolaDragon7.png" alt="dragonBall" width="50" height="50" class="img-thumbnail rounded float-right m-2">
       </div>
   </nav>
 </template>
 
 <script>
+import optionsHeading from '~/assets/optionsHeading.json'
 export default {
     name: 'Encabezado',
     data() {
       return {
-        options: [
-          'Inicio',
-          'Juego',
-          'Personajes'
-        ],
+        options: optionsHeading
       };
     },
 }
 </script>
 
 <style scoped>
+  nav {
+    background-color: black;
+  }
   h2 {
     font-family: fantasy;
   }
