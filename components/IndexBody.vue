5d5b420f90054eccf3df1fcb1eb611a6ce992a94
JesusAlcantara <jesusalcantaragomez11@gmail.com>
2023-03-14 17:43:51 +0100

    finish carousel

--- components/IndexBody.vue
+++ components/IndexBody.vue
@@ -1,5 +1,31 @@
 <template>
-    <div class="container">
+    <div id="carouselIndex" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselIndex" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselIndex" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselIndex" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../public/images/news/budokai4.webp" class="img-carousel" alt="budokai">
+            </div>
+            <div class="carousel-item">
+                <img src="../public/images/news/db-legends.jpg" class="img-carousel" alt="dbLegends">
+            </div>
+            <div class="carousel-item">
+                <img src="../public/images/news/saga-superheroes.webp" class="img-carousel" alt="newSaga">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndex" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselIndex" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- <div class="container">
         <div class="row align-items-start">
             <div class="col">
                 Una de tres columnas
@@ -11,7 +37,7 @@
                 Una de tres columnas
             </div>
         </div>
-    </div>
+    </div> -->
 </template>
 
 <script>
@@ -21,5 +47,14 @@ export default {
 </script>
 
 <style scoped>
-
+    .carousel-control-next,
+    .carousel-control-prev {
+        filter: invert(100%);
+    }
+    .img-carousel {
+        display: block;
+        width: 75%;
+        height: 650px;
+        margin: 10px auto 5px;
+    }
 </style>
