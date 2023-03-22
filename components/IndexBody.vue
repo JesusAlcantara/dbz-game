96607c72bc5a6307d66334296e53ae08e5459a42
JesusAlcantara <jesusalcantaragomez11@gmail.com>
2023-03-22 13:06:41 +0100

    news

--- components/IndexBody.vue
+++ components/IndexBody.vue
@@ -1,9 +1,11 @@
 <template>
     <div v-if="isMobile">
         <carousel-mobile />
+        <news />
     </div>
     <div v-else>
         <carousel-desktop />
+        <news />
     </div>
     <!-- <div class="container">
         <div class="row align-items-start">
@@ -23,10 +25,11 @@
 <script>
 import CarouselDesktop from './carousel/desktop/CarouselDesktop.vue'
 import CarouselMobile from './carousel/mobile/CarouselMobile.vue'
+import News from './news/News.vue';
 import isMobileDevice from '~/helpers/device';
 
 export default {
-    components: { CarouselDesktop, CarouselMobile },
+    components: { CarouselDesktop, CarouselMobile, News },
     data () {
         return {
             isMobile: false
---
+++ components/news/News.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2 class="m-2 fw-bold title"><u>Novedades:</u></h2>
+        <div class="d-flex justify-content-around flex-wrap contenedor">
+            <div
+                v-for="item in news"
+                :key="item.id"
+                class="card m-2"
+                style="width: 18rem;"
+            >
+                <img
+                    :src="getImage(item.image)"
+                    class="card-img-top"
+                    :alt="item.name">
+                <div class="card-body">
+                    <h6>{{ item.category }}</h6>
+                    <hr>
+                    <p>{{ item.description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import news from '~/assets/news.json';
+
+export default {
+    data () {
+        return {
+            news: news
+        }
+    },
+    methods: {
+        getImage(img) {
+            return require(`@/public/images/news/${img}`);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
